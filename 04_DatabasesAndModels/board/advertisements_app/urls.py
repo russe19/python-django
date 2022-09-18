c292d99c4e88071f488d5d27f89c54bdf90708fa
russe <danil.2001.danil.2002@gmail.com>
2022-09-18 16:39:39 +0300

    1

--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("advertisements", views.AdvertisementListView.as_view(), name='advertisement'),
+    path("advertisements/<int:pk>", views.AdvertisementDetailView.as_view(), name='advertisement-detail'),
 
 ]
--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView, ListView, DetailView
+from advertisements_app.models import Advertisement
+
+
+class AdvertisementListView(ListView):
+    model = Advertisement
+    template_name = 'advertisement_list.html'
+    context_object_name = 'advertisement_list'
+    queryset = Advertisement.objects.all()[:5]
+
+class AdvertisementDetailView(DetailView):
+    model = Advertisement
