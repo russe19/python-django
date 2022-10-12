5e7024fd16ebb8bd996cc5169b763845962c0e0e
russe <danil.2001.danil.2002@gmail.com>
2022-10-12 10:16:00 +0300

    2

--- 05_Forms/news/app_news/tests.py
+++ 05_Forms/news/app_news/tests.py
@@ -1,7 +1,7 @@
-# from django.test import TestCase
+from django.test import TestCase
 #
 # # Create your tests here.
-import os
-print(__name__)
-print(os.path.abspath(__file__))
-print(os.path.dirname(os.path.abspath(__file__)))
+# import os
+# print(__name__)
+# print(os.path.abspath(__file__))
+# print(os.path.dirname(os.path.abspath(__file__)))
